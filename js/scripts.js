943337d9b1dff8d31f2fbf557549ae1ccda7e145
Jeff Lai <pjefflai@gmail.com>
2021-10-22 09:54:27 -0700

    WIP: writing initial scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("formOne").submit(function (event) {
+    event.preventDefault();
+    const study = $("study").val();
+    const location = $("location").val();
+    const time = $("time").val();
+    const snack = $("snack").val();
+    const drink = $("drink").val();
+    let result;
+  })
+})
