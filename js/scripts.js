e36cbbf186084e5030857ab0439a181733a38915
Jeff Lai <pjefflai@gmail.com>
2021-10-22 11:09:49 -0700

    Fix result to true, working on making && work

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
   $("form#formOne").submit(function (event) {
     event.preventDefault();
     const study = $("input:radio[name=study]:checked").val();
-    // const location = $("input:radio[name:location]:checked").val();
-    // const time = $("input:radio[name:time]:checked").val();
-    // const snack = $("input:radio[name:snack]:checked").val();
-    // const drink = $("input:radio[name:drink]:checked").val();
+    const location = $("input:radio[name:location]:checked").val();
+    const time = $("input:radio[name:time]:checked").val();
+    const snack = $("input:radio[name:snack]:checked").val();
+    const drink = $("input:radio[name:drink]:checked").val();
     let result;
 
-    if (study === "visual learning") {
+    if (study === "visual" && location === "library" && time === "morning" && snack === "fruits" && drink === "soda") {
       result = "hello";
     }
     else { result = "None"; }
