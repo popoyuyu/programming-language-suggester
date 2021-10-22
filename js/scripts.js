d2808d45f65d18f41fb33e7abdf951ac90f1f0bd
Jeff Lai <pjefflai@gmail.com>
2021-10-22 15:00:58 -0700

    Implement counter system update

--- js/scripts.js
+++ js/scripts.js
@@ -7,62 +7,72 @@ $(document).ready(function () {
     const snack = $("input:radio[name=snack]:checked").val();
     const drink = $("input:radio[name=drink]:checked").val();
     const name = $("#name").val();
-    let result = "";
 
     let cSharpCounter = 0
     let jsCounter = 0
     let pythonCounter = 0
 
     if (study === "visual") {
-      cSharpCounter = cSharpCounter + 1
-    } else if (study === "audio") {
-      jsCounter = jsCounter + 1
-    } else if (study === "handson") {
-      pythonCounter = pythonCounter + 1
+      cSharpCounter = cSharpCounter + 1;
     }
+    else if (study === "audio") {
+      jsCounter = jsCounter + 1;
+    }
+    else if (study === "handson") {
+      pythonCounter = pythonCounter + 1;
+    }
+
     if (location === "library") {
-      cSharpCounter = cSharpCounter + 1
-    } else if (location === "cafe") {
-      cSharpCounter = cSharpCounter + 1
-    } else if (location === "home") {
-      pythonCounter = pythonCounter + 1
+      cSharpCounter = cSharpCounter + 1;
     }
+    else if (location === "cafe") {
+      cSharpCounter = cSharpCounter + 1;
+    }
+    else if (location === "home") {
+      pythonCounter = pythonCounter + 1;
+    }
+
     if (time === "morning") {
-      pythonCounter = pythonCounter + 1
-    } else if (time === "afternoon") {
-      jsCounter = jsCounter + 1
-    } else if (time === "night") {
-      cSharpCounter = cSharpCounter + 1
+      pythonCounter = pythonCounter + 1;
     }
+    else if (time === "afternoon") {
+      jsCounter = jsCounter + 1;
+    }
+    else if (time === "night") {
+      cSharpCounter = cSharpCounter + 1;
+    }
+
     if (snack === "fruits") {
-      cSharpCounter = cSharpCounter + 1
-    } else if (snack === "chips") {
-      jsCounter = jsCounter + 1
-    } else if (snack === "popcorn") {
-      pythonCounter = pythonCounter + 1
+      cSharpCounter = cSharpCounter + 1;
     }
+    else if (snack === "chips") {
+      jsCounter = jsCounter + 1;
+    }
+    else if (snack === "popcorn") {
+      pythonCounter = pythonCounter + 1;
+    }
+
     if (drink === "soda") {
-      cSharpCounter = cSharpCounter + 1
-    } else if (drink === "coffee") {
-      jsCounter = jsCounter + 1
-    } else if (drink === "tea") {
-      pythonCounter = pythonCounter + 1
+      cSharpCounter = cSharpCounter + 1;
     }
-    console.log(cSharpCounter, jsCounter, pythonCounter);
+    else if (drink === "coffee") {
+      jsCounter = jsCounter + 1;
+    }
+    else if (drink === "tea") {
+      pythonCounter = pythonCounter + 1;
+    }
 
-    // if (study === "visual" || location === "library" && time === "morning" && snack === "fruits" && drink === "soda") {
-    //   result = "C#";
-    // }
+    if (cSharpCounter > pythonCounter && cSharpCounter > jsCounter) {
+      result = name + " you should study C#."
+    }
+    else if (pythonCounter > cSharpCounter && pythonCounter > jsCounter) {
+      result = name + " you should study Python."
+    }
+    else if (jsCounter > cSharpCounter && jsCounter > pythonCounter) {
+      result = name + " you should study JavaScript."
+    }
+    else { result = "Inconclusive please redo test." }
 
-    // else if (study === "audio" && location === "cafe" && time === "afternoon" && snack === "chips" && drink === "coffee") {
-    //   result = "JavaScript";
-    // }
-
-    // else if (study === "handson" && location === "home" && time === "night" && snack === "popcorn" && drink === "tea") {
-    //   result = "Python";
-    // }
-
-    result = name + "you should study" + result
     $("#result").text(result);
     $("#result").show();
   });
