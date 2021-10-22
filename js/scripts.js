e003b5f6cfc280388c4d9009a5ab58f4bc7c9afd
Jeff Lai <pjefflai@gmail.com>
2021-10-22 10:50:54 -0700

    still trying to figure out why result wont come out true

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,18 @@ $(document).ready(function () {
   $("form#formOne").submit(function (event) {
     event.preventDefault();
     const study = $("input:radio[name=study]:checked").val();
-    const location = $("input:radio[name:location]:checked").val();
-    const time = $("input:radio[name:time]:checked").val();
-    const snack = $("input:radio[name:snack]:checked").val();
-    const drink = $("input:radio[name:drink]:checked").val();
+    // const location = $("input:radio[name:location]:checked").val();
+    // const time = $("input:radio[name:time]:checked").val();
+    // const snack = $("input:radio[name:snack]:checked").val();
+    // const drink = $("input:radio[name:drink]:checked").val();
     let result;
 
-    if (study === "visual" && location === "library" && time === "morning" && snack === "fruits" && drink === "soda")) {
-    result = "C#";
-  }
+    if (study === "visual learning") {
+      result = "hello";
+    }
     else { result = "None"; }
 
-  $("#result").text("C#");
-});
+    $("#result").text(result);
+    $("#result").show();
+  });
 });
